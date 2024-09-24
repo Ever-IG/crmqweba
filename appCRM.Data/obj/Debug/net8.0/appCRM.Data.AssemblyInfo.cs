26b2b6e1f8d9f36aa0f92f497990128eaa12b9d1
Ever-IG <e.sanchez.sa00@gmail.com>
2024-09-23 23:57:28 -0600

    nuevos cambios para vista del prospecto

--- appCRM.Data/obj/Debug/net8.0/appCRM.Data.AssemblyInfo.cs
+++ appCRM.Data/obj/Debug/net8.0/appCRM.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appCRM.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6815759ce3816cbd642dfca230fb7e2aae4a88ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff2a5482ece87e19dbe1c6f6f1d25a7d4e678de")]
 [assembly: System.Reflection.AssemblyProductAttribute("appCRM.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appCRM.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
